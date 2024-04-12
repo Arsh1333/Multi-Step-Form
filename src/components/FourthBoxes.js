da37f71421537dd54c3425f1126b63124b558f3a
Arsh <pawar.arsh1333@gmail.com>
2024-04-12 16:13:10 +0530

    highlight option in every page

--- src/components/FourthBoxes.js
+++ src/components/FourthBoxes.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 const FourthBoxes = (props) => {
   const { heading, content } = props;
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
   return (
-    <div className="fourth-box border-2 border-gray-400 p-5">
+    <div
+      className={`border ${
+        isClicked
+          ? "border-yellow-600 border-10 border-solid shadow-slate-700"
+          : "border-gray-400"
+      } fourth-box border-2 border-gray-400 p-5`}
+      onClick={handleClick}
+    >
       <div className="box">
         <div className="heading">{props.heading}</div>
         <div className="box-content">{props.content}</div>
--- src/components/Second.js
+++ src/components/Second.js
@@ -1,66 +1,20 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+// import { useState } from "react";
 import MainComponentSecond from "./MainComponentSecond";
+import Options from "./Options";
 
 const Second = (props) => {
-  const [isClicked, setIsClicked] = useState(false);
-
-  const handleClick = () => {
-    setIsClicked(!isClicked);
-  };
+  const iconUrlOne =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSQMZnWTqKuBT9iluwbaHxZeqYIjlaFbLlpQ&s";
   return (
     <div className="second">
       <MainComponentSecond />
-      <div
-        className={`border ${
-          isClicked
-            ? "border-yellow-600 border-10 border-solid shadow-slate-700"
-            : "border-gray-400"
-        } Options flex items-center h-30 w-50 m-20 p-3 justify-center border-6 border-gray p-4`}
-        onClick={handleClick}
-      >
-        <h1>Learning specific skills to advance my career</h1>
-      </div>
-      <div
-        className={`border ${
-          isClicked
-            ? "border-yellow-600 border-10 border-solid shadow-slate-700"
-            : "border-gray-400"
-        } Options flex items-center h-30 w-50 m-20 p-3 justify-center border-6 border-gray p-4`}
-        onClick={handleClick}
-      >
-        <h1>Exploring new topics I'm interested in</h1>
-      </div>
-      <div
-        className={`border ${
-          isClicked
-            ? "border-yellow-600 border-10 border-solid shadow-slate-700"
-            : "border-gray-400"
-        } Options flex items-center h-30 w-50 m-20 p-3 justify-center border-6 border-gray p-4`}
-        onClick={handleClick}
-      >
-        <h1>Refreshing my math foundations</h1>
-      </div>
-      <div
-        className={`border ${
-          isClicked
-            ? "border-yellow-600 border-10 border-solid shadow-slate-700"
-            : "border-gray-400"
-        } Options flex items-center h-30 w-50 m-20 p-3 justify-center border-6 border-gray p-4`}
-        onClick={handleClick}
-      >
-        <h1>Exercising my brain to stay sharp</h1>
-      </div>
-      <div
-        className={`border ${
-          isClicked
-            ? "border-yellow-600 border-10 border-solid shadow-slate-700"
-            : "border-gray-400"
-        } Options flex items-center h-30 w-50 m-20 p-3 justify-center border-6 border-gray p-4`}
-        onClick={handleClick}
-      >
-        <h1>Something else !</h1>
-      </div>
+      <Options description="Student or soon to be enrolled" icon={iconUrlOne} />
+      <Options description="Professional pursuing a career" icon={iconUrlOne} />
+      <Options description="Parent of a school-age child" icon={iconUrlOne} />
+      <Options description="Lifelong Learner" icon={iconUrlOne} />
+      <Options description="Teacher" icon={iconUrlOne} />
+      <Options description="Other" icon={iconUrlOne} />
       <Link to="/third">
         {" "}
         <button className=" md:ml-[600px] ml-[170px] p-3 w-30 rounded-md bg-blue-100">
